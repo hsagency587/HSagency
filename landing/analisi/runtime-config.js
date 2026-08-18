bf692b958c8e30a7b4813f2df0eb4ae2f767038e
hsagency587 <hsagency587@gmail.com>
2026-08-18 12:49:27 +0200

    Update runtime-config.js

--- landing/analisi/runtime-config.js
+++ landing/analisi/runtime-config.js
@@ -2,7 +2,7 @@ window.HS_AGENCY_ANALYSIS_CONFIG = Object.freeze({
   googleMapsApiKey: "AIzaSyAfHXMsZBs7Fj4v-g1lix-nUrcq-BdX-c0",
   emailjs: Object.freeze({
     serviceId: "service_175hhm6",
-    templateId: "template_rxwlxp9",
+    templateId: "template_ntusi9f",
     publicKey: "YQWEfCbwwU7Y7Oo72"
   }),
   // Pixel Meta collegato alla campagna attuale (Events Manager, dataset "HS Agency - Website pixel").
